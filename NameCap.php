ad262d2840a21a293507f57613f36fdfd2707982
Jeremy Law <jeremy.douglas.law@gmail.com>
2013-07-08 13:59:18 -0400

    initial commit

---
+++ NameCap.php
@@ -0,0 +1,69 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * CodeIgniter Name Capitalization Class
+ *
+ * Properly formats an English name string
+ *
+ * @package		CodeIgniter
+ * @subpackage	Libraries
+ * @category	Libraries
+ * @author		Jeremy Law
+ * @link		
+ */
+
+class NameCap {
+
+	/*
+	*	An array to hold the default delimters
+	*/
+	var $delims = array('\'', 'mac', 'mc', '-', 'Mac', 'Mc', '(');
+	var $fixedName;
+	var $CI;
+	
+
+	/*
+	*	On construct add the supplied delimiters to the delimiter array
+	*/
+	public function __construct ($delims = array()) {
+		
+		foreach ($delims as $key => $item) {
+
+			if (!in_array ($item, $this->delims)) {
+
+				array_push ($this->delims, $item);
+
+			}
+
+		}
+		
+	}
+
+	/*
+	*
+	*/
+	public function fixName ($name) {
+
+		$this->fixedName = strtolower ($name); //let's start with a fresh lowercase string
+
+		foreach ($this->delims as $delim) {
+
+			$pos = strpos ($this->fixedName, $delim); //look for the delimiter in the name string
+
+			if ($pos) {
+
+					//if we find the delimiter, let's make the character after it uppercase
+					$this->fixedName[$pos + strlen ($delim)] = strtoupper ($this->fixedName[$pos + strlen ($delim)]);
+
+			}
+
+		}
+
+		//Use ucwords to make the first letter uppercase again
+		return ucwords($this->fixedName);
+
+	}	
+
+}
+
+/* End of file NameCap.php */
